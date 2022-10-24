3c9da69d0854cd7ba242b660ac2a3854690ab842
tanno <94481731+tanno1@users.noreply.github.com>
2022-10-23 18:55:00 -0700

    Logo work, link work, unfinished portfolio pop ups

--- resources/script.js
+++ resources/script.js
@@ -1,54 +1,16 @@
-function $(x) {return document.getElementById(x);} 
-
-//LOOP TYPE SECTION//
-const loopText = [
-    {text: "Test", color: "black"},
-    {text: "Test 2", color: "black"},
-    {text: "Test 3", color: "black"}
-]
-
-$( document ).ready(async function() {
-    LoopWord(loopText, "#change-sentence")
-  });
-
-//TYPING SECTION//
-
-async function typewords(words, eleRef, delay = 100) {
-    const letters = words.split("");
-    let i=0;
-    while(i<letters,length) {
-        await waitForMS(delay);
-        $(eleRef).append(letters[i]);
-        i++
-    }
-    return; 
+//When a user clicks on the div, open the popup//
+function PopUpFunc() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+//Phone Popups//
+function PopUpFunc2() {
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show2");
 }
 
-//DELETE SECTION//
-
-async function deleteWord(eleRef) {
-    const sentence = $(eleRef).html();
-    const letters = sentence.split("");
-    let i = 0;
-    while(letters.length > 0) {
-        await waitForMS(100);
-        letters.pop();
-        $(eleRef).html(letters.join(""));
-    }
-}
-
-async function LoopWord(cartList, eleRef) {
-    var i = 0;
-    while(true) {
-        await typewords(cartList[i].text, eleRef);
-        await waitForMS(1500);
-        await deleteWord(eleRef);
-        await waitForMS(500);
-        i++
-        if(i > cartList.length) {i = 0;}
-    }
-}
-
-function waitForMS(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+//Portfolio Pop ups//
+function portfolio() {
+    var pops = document.getElementById("dirtbike-text");
+    pops.classList.toggle("section-show")
 }
